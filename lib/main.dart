56639b845f6a282de7e7823f9b52b0fa185362b2
Junshen18 <108778342+Junshen18@users.noreply.github.com>
2023-12-29 13:04:25 +0800

    set the screen to vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:habit_tracker/pages/home_page.dart';
 
 void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Lock the screen only can portrait
+  SystemChrome.setPreferredOrientations([ 
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  //Allows your app's content to extend to the edges of the screen, creating a fullscreen
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(const MyApp());
 }
 
